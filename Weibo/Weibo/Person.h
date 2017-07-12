5861a8de6496473c9c4240c8a5ec8733d50bb5f2
chaofengwang <1559878532@qq.com>
2017-07-12 12:07:06 +0800

    添加Person

---
+++ Weibo/Weibo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  Weibo
+//
+//  Created by WCF on 2017/7/12.
+//  Copyright © 2017年 WCF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ Weibo/Weibo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  Weibo
+//
+//  Created by WCF on 2017/7/12.
+//  Copyright © 2017年 WCF. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
